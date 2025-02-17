22fb696b3fdfc0341e542a6b7d6fa06eea4ffef0
manik <maniksarker652@gmail.com>
2025-02-17 16:37:02 +0600

    start intregation

--- src/redux/api/baseApi.jsx
+++ src/redux/api/baseApi.jsx
@@ -1,26 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://api.sellaze.com',
-    // baseUrl: 'http://192.168.10.11:5000',
-    // baseUrl: 'http://167.71.20.155:5000',
-    prepareHeaders: (headers) => {
-        const token = JSON.parse(localStorage.getItem('token'));
-        if (token) {
-            headers.set('Authorization', `Bearer ${token}`);
-        }
-        return headers;
-    },
+  baseUrl: "http://10.0.60.137:7050",
+  // baseUrl: 'http://192.168.10.11:5000',
+  // baseUrl: 'http://167.71.20.155:5000',
+  prepareHeaders: (headers) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
 });
 
 export const baseApi = createApi({
-    reducerPath: 'baseApi',
-    baseQuery: baseQuery,
-    tagTypes: ['overview'],
-    endpoints: () => ({})
+  reducerPath: "baseApi",
+  baseQuery: baseQuery,
+  tagTypes: ["overview"],
+  endpoints: () => ({}),
 });
 
-
-export const imageUrl = 'https://api.sellaze.com/'
+export const imageUrl = "http://10.0.60.137:7050/";
 // export const imageUrl = 'http://192.168.10.11:5000/'
 // export const imageUrl = 'http://192.168.10.11:5000/'
