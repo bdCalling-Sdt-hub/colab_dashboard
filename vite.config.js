b1cb4852429dc6b671432abd1dc4a64882655b56
manik <maniksarker652@gmail.com>
2025-05-25 12:21:31 +0600

    s

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss()],
   server: {
-    // host: "143.110.241.146",
     host: "0.0.0.0",
-    port: "3000",
+    port: 4173,
   },
 });
